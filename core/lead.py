65d7a6e3131aa64ad6f409cedae789644f879a98
Monisha Natarajan <monishanatarajan1506@outlook.com>
2022-07-26 01:32:32 +0000

    Full Feature/Lead source code is now available.
    Handing over the Lead feature to QA team for testing purpose.

---
+++ core/lead.py
@@ -0,0 +1,16 @@
+#Lead feature functionalities
+
+
+# Create Lead
+def create_lead():
+	 print "Created lead successfully. "
+
+#update Lead
+	 def update_lead():
+	 	print "updated Lead successfully"
+
+
+#delete Lead
+	def delete_lead():
+		print "Deleted the lead successfully"
+
