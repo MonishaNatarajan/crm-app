907a314c0b89d79ac19bec6d20af11fd2b4e1a2b
Monisha Natarajan <monishanatarajan1506@outlook.com>
2022-07-26 12:35:38 +0000

    test lead

--- core/lead.py
+++ core/lead.py
@@ -14,6 +14,15 @@ def create_lead():
 	def delete_lead():
 		print "Deleted the lead successfully"
 
+
+
+
+
+
+
+
+
+
 #Test lead
 	def test_lead():
 		print "This is to test the git reset - soft mode"
